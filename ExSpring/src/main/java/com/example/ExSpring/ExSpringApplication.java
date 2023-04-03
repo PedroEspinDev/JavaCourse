e3dcf901b91a9f2fe35af0cd384ebcfe83275b1a
cazatuertos <pedroesping@gmail.com>
2023-04-03 18:12:44 +0200

    Added ExSpring Project

---
+++ ExSpring/src/main/java/com/example/ExSpring/ExSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSpringApplication.class, args);
+	}
+
+}
---
+++ ExSpring/src/test/java/com/example/ExSpring/ExSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
