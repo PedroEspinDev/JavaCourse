76aaf1c27fc561f3a1e273e175b857d22188c959
Cazatuertos <pedroesping@gmail.com>
2023-04-12 19:52:40 +0200

    Another day moving on

---
+++ Spring-Boot Course/spring-boot-web/src/main/java/com/ideabag/springboot/web/app/controllers/EjemploParamsController.java
@@ -0,0 +1,44 @@
+package com.ideabag.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+    @GetMapping("/")
+    public String index() {
+        return "params/index";
+    }
+
+    @GetMapping("/string")
+    public String params(@RequestParam(name = "texto", required = false, defaultValue = "No hay valores") String texto, Model model) {
+        model.addAttribute("resultado", "El texto enviado es: " + texto);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String params(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+        model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el número es '" + numero + "'");
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String params(HttpServletRequest request, Model model) {
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+        try {
+            numero = Integer.parseInt(request.getParameter("numero"));
+        } catch (NumberFormatException e) {
+            numero = 0;
+        }
+        model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el número es '" + numero + "'");
+        return "params/ver";
+    }
+
+}
--- Spring-Boot Course/spring-boot-web/src/main/java/com/ideabag/springboot/web/app/controllers/IndexController.java
+++ Spring-Boot Course/spring-boot-web/src/main/java/com/ideabag/springboot/web/app/controllers/IndexController.java
@@ -1,14 +1,56 @@
 package com.ideabag.springboot.web.app.controllers;
 
+import com.ideabag.springboot.web.app.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("titulo", "hola Spring Framework cn Model!");
+        return "index";
+    }
 
-        return "";
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        User user = new User();
+        user.setName("Pedro");
+        user.setSurname("Espin");
+        user.setEmail("PedroEspin@correo.com");
+
+        model.addAttribute("user", user);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(user.getName()));
+        return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model) {
+        List<User> users = Arrays.asList(new User("Pedro", "Espin", "PedroEspin@correo.com"),
+                new User("Pablo", "Lopez", "PabloLopez@correo.com"),
+                new User("Fran", "Lopez", "FranLopez@correo.com"),
+                new User("Carlos", "Garcia", "CarlosGarcia@correo.com"));
+
+        model.addAttribute("titulo", "Listado de usuarios");
+
+        return "listar";
+    }
+    @ModelAttribute("usuarios")
+    public List<User> poblarUsuarios(){
+        List<User> users = Arrays.asList(new User("Pedro", "Espin", "PedroEspin@correo.com"),
+                new User("Pablo", "Lopez", "PabloLopez@correo.com"),
+                new User("Fran", "Lopez", "FranLopez@correo.com"),
+                new User("Carlos", "Garcia", "CarlosGarcia@correo.com"));
+
+        return users;
     }
 }
