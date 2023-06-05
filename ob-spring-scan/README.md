7de8edfcc4f3d64a8de0d146100ffbabd735d6d9
Pedro Espín <muertenegrasanpedro@hotmail.com>
2023-06-05 10:20:23 +0200

    Cleaning old files

--- ob-spring-scan/README.md
+++
@@ -1,5 +0,0 @@
-
-* Spring Data JPA
-    * JPA
-        * Hibernate
-          * JDBC
--- ob-springdatajpa/src/main/java/com/example/obspringdatajpa/ICocheRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.obspringdatajpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICocheRepository extends JpaRepository<Coche, Long> {
-}
